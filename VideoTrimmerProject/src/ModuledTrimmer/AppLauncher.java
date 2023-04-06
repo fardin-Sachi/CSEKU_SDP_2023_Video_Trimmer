f735a21ea1b1a59606b8877adc4820e9dc256de1
Fardin Ahsan <fardin2002@cseku.ac.bd>
2023-04-06 13:02:33 +0600

    Added below things:
    Added Video Module. Although trimmed video is not showing in the UI.
    Followed coding convention. Created Class separately for UI, Trim, also
    added extra class for the main class.

---
+++ VideoTrimmerProject/src/ModuledTrimmer/AppLauncher.java
@@ -0,0 +1,9 @@
+package ModuledTrimmer;
+
+import javafx.application.Application;
+
+public class AppLauncher{
+    public static void main(String[] args) {
+        Application.launch(Interface.class, args);
+    }
+}
--- VideoTrimmerProject/src/trimmer/Trim.java
+++ VideoTrimmerProject/src/trimmer/Trim.java
@@ -6,6 +6,7 @@ public class Trim{
     public Trim(double startTime, double endTime){
         if (startTime<0 || endTime<0 || startTime>=endTime)
             throw new IllegalArgumentException("Invalid Start/End Time for Trim");
+        
         this.startTime = startTime;
         this.endTime = endTime;
     }
